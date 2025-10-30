11821544b097f7286638a885534a5f7c73b92880
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2025-10-31 02:02:00 +0330

    feat: :sparkles: Add virtual swiper & improve video performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState, type RefObject } from "react";
 import Hls from "hls.js";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Virtual } from "swiper/modules";
 
 const videos = [
   "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
@@ -7,124 +9,137 @@ const videos = [
   "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
   "https://test-streams.mux.dev/tos_ismc/main.m3u8",
   "https://test-streams.mux.dev/pts_shift/master.m3u8",
-  // "https://www.w3schools.com/tags/movie.mp4",
-  // "https://www.pexels.com/download/video/3209663/",
-  // "https://filesamples.com/samples/video/mp4/sample_640x360.mp4",
-  // "https://www.pexels.com/download/video/3195443/",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://test-streams.mux.dev/test_001/stream.m3u8",
+  "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
+  "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+  "https://test-streams.mux.dev/pts_shift/master.m3u8",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://test-streams.mux.dev/test_001/stream.m3u8",
+  "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
+  "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+  "https://test-streams.mux.dev/pts_shift/master.m3u8",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://test-streams.mux.dev/test_001/stream.m3u8",
+  "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
+  "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+  "https://test-streams.mux.dev/pts_shift/master.m3u8",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://test-streams.mux.dev/test_001/stream.m3u8",
+  "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
+  "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+  "https://test-streams.mux.dev/pts_shift/master.m3u8",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://test-streams.mux.dev/test_001/stream.m3u8",
+  "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
+  "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+  "https://test-streams.mux.dev/pts_shift/master.m3u8",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://test-streams.mux.dev/test_001/stream.m3u8",
+  "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
+  "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+  "https://test-streams.mux.dev/pts_shift/master.m3u8",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://test-streams.mux.dev/test_001/stream.m3u8",
+  "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
+  "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+  "https://test-streams.mux.dev/pts_shift/master.m3u8",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://test-streams.mux.dev/test_001/stream.m3u8",
+  "https://test-streams.mux.dev/dai-discontinuity-deltatre/manifest.m3u8",
+  "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+  "https://test-streams.mux.dev/pts_shift/master.m3u8",
 ];
 
 export default function Reels() {
   const refs = useRef<(HTMLVideoElement | null)[]>([]);
   const [activeIndex, setActiveIndex] = useState(0);
+  console.log(
+    "Active HLS instances:",
+    refs.current.filter((v) => v && !v.paused).length
+  );
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        overflowY: "scroll",
-        scrollSnapType: "y mandatory",
+    <Swiper
+      direction="vertical"
+      modules={[Virtual]}
+      slidesPerView={1}
+      virtual={{
+        addSlidesBefore: 5,
+        addSlidesAfter: 5,
       }}
+      mousewheel
+      onSlideChange={(swiper) => {
+        setActiveIndex(swiper.activeIndex);
+      }}
+      style={{ width: "100%", height: "100vh" }}
+      resistanceRatio={0}
     >
-      {videos.map((src, i) => (
-        <Reel
-          key={i}
-          src={src}
-          index={i}
-          setActiveIndex={setActiveIndex}
-          refs={refs}
-          activeIndex={activeIndex}
-        />
+      {videos.map((src, index) => (
+        <SwiperSlide key={index} virtualIndex={index}>
+          <Reel activeIndex={activeIndex} src={src} index={index} refs={refs} />
+        </SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   );
 }
 
 interface ReelProps {
   src: string;
   index: number;
-  setActiveIndex: (index: number) => void;
   activeIndex: number;
   refs: RefObject<(HTMLVideoElement | null)[]>;
 }
 
-function Reel({ index, src, refs, activeIndex, setActiveIndex }: ReelProps) {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const shouldRender = Math.abs(activeIndex - index) <= 1;
-  console.log({ activeIndex, index });
+function Reel({ index, src, refs, activeIndex }: ReelProps) {
+  const hlsRef = useRef<Hls | null>(null);
 
   useEffect(() => {
-    const video = refs.current?.[index];
-    if (!video) return;
+    const videoEl = refs.current?.[index];
+    if (!videoEl) return;
 
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        const visible = entry.isIntersecting;
-        console.log({ index, visible });
-
-        setIsVisible(visible);
-
-        if (isVisible) {
-          setActiveIndex(index);
-          video.play().catch((e) => {
-            console.log({ e });
-          });
-        } else {
-          video.pause();
-        }
-      },
-      { threshold: 1 }
-    );
-
-    observer.observe(video);
-    return () => observer.disconnect();
-  }, [index, refs, setActiveIndex, isVisible]);
-
-  useEffect(() => {
-    const videoSrc = src;
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(videoSrc);
-      hls.attachMedia(refs.current?.[index]);
+    if (!hlsRef.current && index === activeIndex) {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(src);
+        hls.attachMedia(videoEl);
+        hlsRef.current = hls;
+      }
     }
-  }, [index, refs, src]);
-  // if (!shouldRender) {
-  //   return (
-  //     <div
-  //       style={{
-  //         height: "100vh",
-  //         scrollSnapAlign: "start",
-  //         background: "#000",
-  //       }}
-  //     />
-  //   );
-  // }
+
+    if (index === activeIndex) {
+      videoEl.play().catch(() => {});
+    } else {
+      videoEl.pause();
+      hlsRef.current?.stopLoad();
+    }
+
+    return () => {
+      if (Math.abs(activeIndex - index) > 1 && hlsRef.current) {
+        hlsRef.current.destroy();
+        hlsRef.current = null;
+      }
+    };
+  }, [index, activeIndex, refs, src]);
+
+  const shouldPreload = Math.abs(activeIndex - index) <= 1;
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        scrollSnapAlign: "start",
-        position: "relative",
+    <video
+      ref={(el) => {
+        refs.current[index] = el;
       }}
-    >
-      <video
-        ref={(el) => {
-          refs.current[index] = el;
-        }}
-        src={shouldRender ? src : "none"}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "contain",
-          background: "#000",
-        }}
-        muted
-        loop
-        playsInline
-        preload={shouldRender ? "auto" : "none"}
-        controls
-      />
-    </div>
+      style={{
+        width: "50%",
+        height: "50%",
+        objectFit: "contain",
+        background: "#000",
+      }}
+      muted
+      loop
+      playsInline
+      preload={shouldPreload ? "preload" : "none"}
+      controls
+    />
   );
 }
